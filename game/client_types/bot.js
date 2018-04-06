a490082c878c9fde53786912a59f66c459e6e396
Stefano Balietti <futur.dorko@gmail.com>
2018-04-05 22:26:12 -0400

    bot

--- game/client_types/bot.js
+++ game/client_types/bot.js
@@ -1,16 +1,50 @@
 /**
- * # Bot type implementation of the game stages
- * Copyright(c) 2017 Stefano Balietti <ste@nodegame.org>
+ * # Bot type.
+ * Copyright(c) 2018 Stefano Balietti <ste@nodegame.org>
  * MIT Licensed
  *
  * http://www.nodegame.org
  * ---
  */
+var ngc = require('nodegame-client');
+var J = ngc.JSUS;
 
 module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
 
-    // Must implement the stages here.
+    var channel = gameRoom.channel;
+    var logic = gameRoom.node;
 
-    // ...
+    stager.extendAllSteps(function(o) {
+        o.cb = function() {
+            var node, stepObj, id;
+            stepObj = this.getCurrentStepObj();
+            id = stepObj.id;
+            node = this.node;
 
+            node.timer.randomDone(2000);
+        };
+        return o;
+    });
+
+    stager.extendStep('game', {
+        roles: {
+            OBSERVER: {
+                cb: function() {
+                    this.node.timer.randomDone();
+                }
+            },
+            DICTATOR: {
+                cb: function() {
+                    var node = this.node;
+                    node.on('PLAYING', function() {
+                        node.timer.randomExec(function() {
+                            node.done({
+                                offer: J.randomInt(-1,100)
+                            });
+                        });
+                    });
+                }
+            }
+        }
+    });
 };
